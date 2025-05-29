dea611bb24ba0599fae4252b665fd7ce062d51db
Rob Gaddi <rgaddi@highlandtechnology.com>
2025-05-29 10:26:49 -0700

    Updated the ThreadedTaskQueue for universal error handling, and to allow the threads to terminate rather than run daemonically.

--- linkrottie/taskqueue.py
+++ linkrottie/taskqueue.py
@@ -47,28 +47,22 @@ class SingleTaskQueue:
         self._next_task += 1
         self._queue.append((task, desc, args, kwargs))
 
-    def runnext(self):
-        """Executes the next event from the queue.
-        
-        Raises IndexError if the queue is empty.
-        """
-        
-        (task, desc, args, kwargs) = self._queue.pop(0)
-        log.debug("Executing {%s,%s,%s}", desc, args, kwargs)
-        task(*args, **kwargs)
-        log.debug("Completed {%s}", desc)
-    
     def runall(self):
         """Executes all events in the queue.
         
         This includes new events added to the queue by events in the queue.
         """
         while self._queue:
-            self.runnext()
+            (task, desc, args, kwargs) = self._queue.pop(0)
+            log.debug("Executing {%s,%s,%s}", desc, args, kwargs)
+            task(*args, **kwargs)
+            log.debug("Completed {%s}", desc)
             
     def __len__(self):
         return len(self._queue)
 
+class _TerminateThread(Exception):
+    pass
 
 class ThreadedTaskQueue:
     """Multi-threaded implementation of a TaskQueue.
@@ -83,7 +77,7 @@ class ThreadedTaskQueue:
         self._queue = queue.Queue()
         self._next_task = 1
         self._threads = [
-            threading.Thread(target=self._runthread, daemon=True)
+            threading.Thread(target=self._runthread, daemon=False)
                 for _ in range(max_tasks)
         ]
     
@@ -104,35 +98,50 @@ class ThreadedTaskQueue:
             self._next_task += 1
         self._queue.put((task, desc, args, kwargs))
 
-    def runnext(self):
-        """Executes the next event from the queue."""
-        
-        (task, desc, args, kwargs) = self._queue.get()
-        log.debug("Executing {%s,%s,%s}", desc, args, kwargs)
-        task(*args, **kwargs)
-        self._queue.task_done()
-        log.debug("Completed {%s}, %d items in queue", desc, len(self))
+    def _killthread(self):
+        raise _TerminateThread()
 
     def _runthread(self):
         """Executes queue events while possible."""
 
         while True:
-            self.runnext()
+            try:
+                (task, desc, args, kwargs) = self._queue.get()
+                log.debug("Executing {%s,%s,%s}", desc, args, kwargs)
+                task(*args, **kwargs)
+            except _TerminateThread:
+                break
+            except Exception:
+                log.exception('unhandled exception')
+            finally:
+                self._queue.task_done()
+                log.debug("Completed {%s}, %d items in queue", desc, len(self))
+        
+        log.debug('Terminating thread')
     
     def runall(self):
-        """Executes all events in the queue.
+        """Executes all events in the queue.  Run from main thread.
         
         This includes new events added to the queue by events in the queue.
         """
         
+        # Run all the threads, and the entire queue through to completion
         for t in self._threads:
             t.start()
         self._queue.join()
+
+        # Send all the threads a termination notice
+        for n, t in enumerate(self._threads):
+            self.append(self._killthread, desc=f'Terminate {n}')
+        
+        # And wait for them to complete
+        for t in self._threads:
+            t.join()
             
     def __len__(self):
         return self._queue.qsize()
 
-_tq = ThreadedTaskQueue()   
+_tq = ThreadedTaskQueue()
 
 def taskqueue():
     """Return the global TaskQueue."""
